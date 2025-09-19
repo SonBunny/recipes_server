91df555662f6ce2a4b1f8d5ec9ec0ee2042eff08
SonBunny <159861552+SonBunny@users.noreply.github.com>
2025-09-20 11:09:18 +1200

    Update index.js

--- user_auth_service/index.js
+++ user_auth_service/index.js
@@ -16,7 +16,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 //enabling cors to handle requests from different origin
 app.use(cors({
-  origin: 'http://192.168.1.108:8081', // Your gateway URL
+  origin: 'https://recipegenerator--psin2hviw0.expo.app', // Your gateway URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
 }));
